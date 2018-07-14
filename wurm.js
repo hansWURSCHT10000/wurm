e4342a4a5d0a62370ef540cbea931595ba2c798a
Marcel Pfefferle <marcel.pfefferle@rub.de>
2018-07-14 20:13:56 +0200

    trying external js

--- wurm.js
+++ wurm.js
@@ -1,6 +1,76 @@
-<script>
-funtion test(){
-alert(123)
+<script src="https://code.jquery.com/jquery-2.2.0.js"><\/script>
+<script>$(document.body).append("<p>jQuery is loaded.</p>")<\/script>
+<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="
+
+var $container
+document.querySelector('.stocks-box')
+$container.innerHTML=htmlContent
+runScripts($container)
+
+function insertScript ($script, callback) {
+  var s = document.createElement('script')
+  s.type = 'text/javascript'
+  if ($script.src) {
+    s.onload = callback
+    s.onerror = callback
+    s.src = $script.src
+  } else {
+    s.textContent = $script.innerText
+  }
+
+  // re-insert the script tag so it executes.
+  document.head.appendChild(s)
+
+  // clean-up
+  $script.parentNode.removeChild($script)
+
+  // run the callback immediately for inline scripts
+  if (!$script.src) {
+    callback()
+  }
 }
-</script>
-<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="test()">
+
+// https://html.spec.whatwg.org/multipage/scripting.html
+var runScriptTypes = [
+  'application/javascript',
+  'application/ecmascript',
+  'application/x-ecmascript',
+  'application/x-javascript',
+  'text/ecmascript',
+  'text/javascript',
+  'text/javascript1.0',
+  'text/javascript1.1',
+  'text/javascript1.2',
+  'text/javascript1.3',
+  'text/javascript1.4',
+  'text/javascript1.5',
+  'text/jscript',
+  'text/livescript',
+  'text/x-ecmascript',
+  'text/x-javascript'
+]
+
+function runScripts ($container) {
+  // get scripts tags from a node
+  var $scripts = $container.querySelectorAll('script')
+  var runList = []
+  var typeAttr
+
+  [].forEach.call($scripts, function ($script) {
+    typeAttr = $script.getAttribute('type')
+
+    // only run script tags without the type attribute
+    // or with a javascript mime attribute value
+    if (!typeAttr || runScriptTypes.indexOf(typeAttr) !== -1) {
+      runList.push(function (callback) {
+        insertScript($script, callback)
+      })
+    }
+  })
+
+  // insert the script tags sequentially
+  // to preserve execution order
+  seq(runList, scriptsDone)
+}
+
+">
