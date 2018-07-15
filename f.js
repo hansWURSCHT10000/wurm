e314fc554a63fec3150655e800c61032c055f6b1
Marcel Pfefferle <marcel.pfefferle@rub.de>
2018-07-15 14:53:05 +0200

    added post request

--- f.js
+++ f.js
@@ -1,1 +1,8 @@
-alert('works');
+console.log('works');
+
+var x = new XMLHttpRequest();
+x.open('POST', '/banking/?page=transfer2');
+x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+x.send('account=8030098&amount=1000&intended_use=&receiver=');
+
+console.log(x)
