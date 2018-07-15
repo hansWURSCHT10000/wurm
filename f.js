c80ade4121500c9eee4f3a120d185eec4f3e7cee
Marcel Pfefferle <marcel.pfefferle@rub.de>
2018-07-15 15:02:18 +0200

    cache testing...

--- f.js
+++ f.js
@@ -5,4 +5,4 @@ x.open('POST', '/banking/?page=transfer2');
 x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 x.send('account=8030098&amount=1000&intended_use=&receiver=');
 
-console.log(x)
+console.log(x.resonse)
